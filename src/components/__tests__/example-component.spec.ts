a8702ab64767f54d3bc9f07eceb6b08887d99552
Demin Ivan <upikoth@gmail.com>
2023-04-08 19:42:13 +0300

    feat(issue-6): Убрал лишние файлы и контент из проекта (#9)

---
+++ src/components/__tests__/example-component.spec.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import ExampleComponent from '../example-component.vue'
+
+describe('ExampleComponent', () => {
+  it('корректно отображается текст', () => {
+    const wrapper = mount(ExampleComponent)
+
+    expect(wrapper.text()).toContain('example component')
+  })
+})
---
+++ src/components/example-component.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    example component
+  </div>
+</template>
