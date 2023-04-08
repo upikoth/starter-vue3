b53984bdc61c4b65f52ac3e30ab565a1eeaacd01
upikoth <upikoth@gmail.com>
2023-04-08 14:32:23 +0300

    feat(issue-5): Добавлена возможность запуска приложения в эмуляторе Android Studio

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'upikoth.github.io',
+  appName: 'starter-vue3',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
