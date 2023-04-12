81a2bdf1ae57d6c9262f38a8beb6d2ed2eeffb18
upikoth <upikoth@gmail.com>
2023-04-12 10:35:32 +0300

    feat(issue-14): добавил документацию в проект

--- src/views/documentation-view.vue
+++ src/views/documentation-view.vue
@@ -5,7 +5,25 @@ import {
 </script>
 
 <template>
-	<ion-page class="ion-padding">
-		Документация
+	<ion-page
+		class="documentation-view"
+	>
+		<iframe
+			class="documentation-view__iframe"
+			src="https://upikoth.github.io/starter-docs/"
+			title="Документация"
+		/>
 	</ion-page>
 </template>
+
+<style lang="scss" scoped>
+.documentation-view {
+
+	&__iframe {
+		display: flex;
+		flex-grow: 1;
+		width: 100%;
+		border: none;
+	}
+}
+</style>
