8e7a583fd6f94c111deb464a4c3ee3ef4f9660db
upikoth <upikoth@gmail.com>
2024-03-03 13:12:29 +0300

    chore: добавил простой пример с e2e тестами

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -2,7 +2,8 @@
 
 describe('My First Test', () => {
 	it('visits the app root url', () => {
+		cy.intercept('GET', '/api/v1/session', { fixture: 'example.json', statusCode: 401 })
 		cy.visit('/')
-		cy.contains('h1', 'You did it!')
+		cy.contains('.text-h6', 'Вход в личный кабинет')
 	})
 })
