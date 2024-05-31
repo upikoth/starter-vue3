bd47b26ee2411664ec4ee5eeeb096657a4429aa5
upikoth <upikoth@gmail.com>
2024-05-31 22:05:38 +0300

    chore: поправил тесты

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -2,6 +2,8 @@
 
 describe('My First Test', () => {
 	it('visits the app root url', () => {
+		cy.intercept('GET', '/environment.json?*', { fixture: 'environment.json', statusCode: 200 })
+
 		cy.visit('/')
 
 		cy.contains('.text-h6', 'Вход в личный кабинет')
